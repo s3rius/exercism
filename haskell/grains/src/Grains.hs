2546a8ef33f89a11279948c3b79c0184df297a77
Paul <win10@list.ru>
2018-05-15 20:54:15 +0400

    Updated solution for grains problem

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,12 +1,9 @@
 module Grains (square, total) where
+import Data.Maybe
 
 square :: Integer -> Maybe Integer
 square n | n < 1 || n > 64 = Nothing
          | otherwise = Just(2^(n-1))
 
 total :: Integer
-total = calc 1 65
- where calc i n | i < n = unwrap(square i) + calc (i+1) n
-                | otherwise = 0
-       unwrap Nothing = 0
-       unwrap (Just a) = a
+total = 2^64-1

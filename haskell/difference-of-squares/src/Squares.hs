db974373d5d01057ac9c8951d072aeb177b3c888
Paul <win10@list.ru>
2018-05-15 19:09:21 +0400

    Updated solution for diff of squares problem.

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -4,11 +4,7 @@ difference :: Integral a => a -> a
 difference n = (squareOfSums n) - (sumOfSquares n)  
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = (sum_h 1 n)^2
-    where sum_h i n | i < n = i + sum_h (i+1) n
-                    | otherwise = n
+squareOfSums n = (sum [1 .. n])^2
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = s_sum_h 1 n
-    where s_sum_h i n | i < n = (i^2) + s_sum_h (i+1) n
-                      | otherwise = n^2
+sumOfSquares n = sum $ map (\el -> el^2) [1 .. n]
